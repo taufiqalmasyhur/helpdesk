71cde51595f5e610b941c251c4285fa0f22b3d75
taufiqalmasyhur <almasyhur.taufiq@gmail.com>
2017-08-08 18:13:57 +0700

    add dependencies
    bootstrap, data table

--- application/controllers/engineer.php
+++ application/controllers/engineer.php
@@ -1,12 +1,15 @@
 <?php
-    /**
-     * 
-     */
-    class Engineer extends CI_Controller {
-        
-        function index() {
-            
-        }
-    }
-    
+/**
+ *
+ */
+class Engineer extends CI_Controller {
+
+	function index() {
+		if ($this -> session -> userdata('logged_in'))
+			redirect("engineer/dashboard");
+		else
+			redirect('login');
+	}
+
+}
 ?>
