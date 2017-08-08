5d21e9b17d3983ceb65ca2274173b96dc95262ff
Febrial Pratama <febriyal1996@gmail.com>
2017-08-08 15:18:24 +0700

    Adding empty class
    Admin and user class

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -7,8 +7,19 @@
      */
     class User extends CI_Controller {
         
-        public function index() {
+        public function index() 
+        {
             
+        }
+
+        public function do_InsertRequest()
+        {
+
+        }
+
+        public function do_Comment()
+        {
+        	
         }
 		
 		public function do_Logout()
